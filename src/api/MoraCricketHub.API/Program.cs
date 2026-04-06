18383429e90dd969cfb749bf112119620fe89195
MudusarithaGangamina <gangaminakumara@gmail.com>
2026-04-06 12:00:04 +0530

    chore: scaffold .NET 10 API with Clean Architecture structure

---
+++ src/api/MoraCricketHub.API/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+        policy.WithOrigins(
+            builder.Configuration["Frontend:Url"] ?? "http://localhost:5173")
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowFrontend");
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
---
+++ src/api/MoraCricketHub.Application/AssemblyMarker.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoraCricketHub.Application
+{
+    public abstract class AssemblyMarker { }
+}
